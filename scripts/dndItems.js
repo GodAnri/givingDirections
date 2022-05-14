573be170ed4337594f1fcd2c8d9d103a507c6541
Mario <e11777706@student.tuwien.ac.at>
2022-05-14 16:23:43 +0200

    finish logic for dnd game
    dnd game now works in principle.
    still missing more phrases and all images

---
+++ scripts/dndItems.js
@@ -0,0 +1,16 @@
+export function getDndItems() {
+    return [
+            {id: "01", image:"img01.png", text: "turn left (1)"},
+            {id: "02", image:"img02.png", text: "turn right (2)"},
+            {id: "03", image:"img03.png", text: "go straight ahead (3)"},
+            {id: "04", image:"img04.png", text: "go past the bus stop (4)"},
+            {id: "05", image:"img05.png", text: "take the second right (5)"},
+            {id: "06", image:"img06.png", text: "go across the bridge (6)"},
+            {id: "07", image:"img07.png", text: "turn around (7)"},
+            {id: "08", image:"img08.png", text: "placeholder (8)"},
+            {id: "09", image:"img09.png", text: "placeholder (9)"},
+            {id: "10", image:"img10.png", text: "placeholder (10)"},
+            {id: "11", image:"img11.png", text: "placeholder (11)"},
+            {id: "12", image:"img12.png", text: "placeholder (12)"}
+        ]
+}
